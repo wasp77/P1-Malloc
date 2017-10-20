f5e3bb7cf9d9d47d86a5f8cae5c330c20025a32d
John Dupuis <wasp7@johns-mbp.home>
2017-10-20 07:14:56 -0400

    tightened up code

--- code/myalloc.c
+++ code/myalloc.c
@@ -3,13 +3,14 @@
 #include <sys/time.h>
 #include <stddef.h>
 #include <sys/mman.h>
+#include <pthread.h>
 #include <errno.h>
 #include <string.h>
 #include "myalloc.h"
 
 #define word_size 8
-#define tag_size 16
-#define min_size 48			//must be large enough to store the list pointers in
+#define tag_size 16     //Two words in a tag
+#define min_size 48			//must be large enough to store the list pointers plus header and footer
 #define page_size 4096
 
 typedef struct block_info {
@@ -22,10 +23,14 @@ typedef struct tag {
 	unsigned int free;
 } tag;
 
-block_info *head;
-void* mem_start;
-void* mem_end;
+pthread_mutex_t malloc_lock;
+block_info *head;             //Holds the head of the explicit list
+void* mem_start;              //Used to check if a free block is at the start of memory
+void* mem_end;                //Used to check if a free block is at the end of memory
 unsigned int init = 0;
+
+void split(tag *current_head, tag *current_foot, tag *next_head, tag *next_foot, block_info *current_info, block_info *next_info, int size);
+void removeLinks (block_info *link_ptrs);
 
 void *myalloc(int size){
 	if (!size) return NULL;
@@ -35,15 +40,23 @@ void *myalloc(int size){
   tag *foot_tag;
 	block_info *head_info;
 
-	if (size < (tag_size)) {
-		size = (tag_size);
+  //Make sure the size is large enough to fit the two pointers
+	if (size < tag_size) {
+		size = tag_size;
 	}
 
+  //Make sure the size is a multiple of the word size for alignment purposes
+  if (size % word_size) {
+    size = size + (word_size - (size % word_size));
+  }
+
+  pthread_mutex_lock(&malloc_lock);
   if (!init) {
     init = 1;
     size_t actual_size = (tag_size * 2) + size;
     void* block = mmap(NULL, page_size * 5000, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
     if (block == (void *) -1) {
+      pthread_mutex_unlock(&malloc_lock);
       printf("%s\n", strerror(errno));
       return NULL;
     }
@@ -51,7 +64,7 @@ void *myalloc(int size){
     mem_start = block;
     mem_end = (char*)block + page_size * 5000;
     start_tag = block;
-    void* block_start = (char*) start_tag + (tag_size);
+    void* block_start = (char*)block + tag_size;
     start_tag->size = (page_size * 5000) - (tag_size * 2);
     start_tag->free = 0;
 
@@ -68,7 +81,7 @@ void *myalloc(int size){
       start_foot->size = size;
       start_foot->free = 0;
     }
-    //printf("Initialised %p\n", );
+    pthread_mutex_unlock(&malloc_lock);
     return block_start;
   }
 
@@ -78,45 +91,11 @@ void *myalloc(int size){
     start_foot = (char*)curr + start_tag->size;
 		if (start_tag->size >= size) {
 			if ((start_tag->size - size) >= min_size) {
-				head_tag = (char*)curr + (size + tag_size);
-				head_tag->size = start_tag->size - (size + tag_size * 2);
-				head_tag->free = 1;
-        foot_tag = start_foot;
-        foot_tag->size = head_tag->size;
-        foot_tag->free = 1;
-				start_tag->size = size;
-        start_tag->free = 0;
-        start_foot = (char*)head_tag - tag_size;
-        start_foot->size = size;
-        start_foot->free = 0;
-        head_info = (char*)head_tag + tag_size;
-				if (curr->next) {
-					head_info->next = curr->next;
-					curr->next->prev = head_info;
-				} else {
-					head_info->next = NULL;
-				}
-				if (curr->prev) {
-					head_info->prev = curr->prev;
-					curr->prev->next = head_info;
-				} else {                           //Nothing previous means it is the head
-					head_info->prev = NULL;
-          head = head_info;
-				}
+        split(start_tag, start_foot, head_tag, foot_tag, curr, head_info, size);
 			} else {
-        if (curr->next && curr->prev) {
-          curr->next->prev = curr->prev;
-          curr->prev->next = curr->next;
-        } else if (curr->next && !curr->prev) {
-          curr->next->prev = NULL;
-          head = curr->next;
-        } else if (!curr->next && curr->prev) {
-          curr->prev->next = NULL;
-        } else {
-          printf("we screwed up \n");
-          return NULL;
-        }
+        removeLinks(curr);
       }
+      pthread_mutex_unlock(&malloc_lock);
 			return (void*) (curr);
 		}
 		curr = curr->next;
@@ -135,118 +114,56 @@ void myfree(void *ptr){
   tag *prev_tag;
   size_t total_size;
 
+  pthread_mutex_lock(&malloc_lock);
   //Get the three structs for the current block
   current_head = (char*)ptr - tag_size;
   current_foot = (char*)ptr + current_head->size;
   current_info = ptr;
-
   //Check if the current block is at the start or end of memory
   if (current_head == mem_start) {
-    printf("head of memory\n");
     next_tag = (char*)current_foot + tag_size;
     next_info = (char*)next_tag + tag_size;
-
     if (next_tag->free) {
       total_size = current_head->size + next_tag->size + (tag_size * 2);
-      if (next_info->next && next_info->prev) {
-        next_info->next->prev = next_info->prev;
-        next_info->prev->next = next_info->next;
-      } else if (next_info->next && !next_info->prev) {
-        next_info->next->prev = NULL;
-        head = next_info->next;
-      } else if (!next_info->next && next_info->prev) {
-        next_info->prev->next = NULL;
-      }
+      removeLinks(next_info);
       current_head->size = total_size;
       current_foot = (char*)next_info + next_tag->size;
       current_foot->size = total_size;
   }
-
-
   } else if (((char*)current_foot + tag_size) == mem_end) {
-    printf("end of memory\n");
     prev_tag = (char*)current_head - tag_size;
     prev_info = (char*)prev_tag - prev_tag->size;
-
     if (prev_tag->free) {
       total_size = current_head->size + prev_tag->size + (tag_size * 2);
-      if (prev_info->next && prev_info->prev) {
-        prev_info->next->prev = prev_info->prev;
-        prev_info->prev->next = prev_info->next;
-      } else if (prev_info->next && !prev_info->prev) {
-        prev_info->next->prev = NULL;
-        head = prev_info->next;
-      } else if (!prev_info->next && prev_info->prev) {
-        prev_info->prev->next = NULL;
-      }
+      removeLinks(prev_info);
       current_head = (char*)prev_info - tag_size;
       current_head->size = total_size;
       current_foot->size = total_size;
       current_info = prev_info;
     }
-
-
   } else {
-    printf("not end of beginning\n");
     next_tag = (char*)current_foot + tag_size;
     next_info = (char*)next_tag + tag_size;
     prev_tag = (char*)current_head - tag_size;
     prev_info = (char*)prev_tag - prev_tag->size;
-
-
     if (prev_tag->free && next_tag->free) {
       total_size = current_head->size + prev_tag->size + next_tag->size + (tag_size * 4);
-      if (prev_info->next && prev_info->prev) {
-        prev_info->next->prev = prev_info->prev;
-        prev_info->prev->next = prev_info->next;
-      } else if (prev_info->next && !prev_info->prev) {
-        prev_info->next->prev = NULL;
-        head = prev_info->next;
-      } else if (!prev_info->next && prev_info->prev) {
-        prev_info->prev->next = NULL;
-      }
-
-      if (next_info->next && next_info->prev) {
-        next_info->next->prev = next_info->prev;
-        next_info->prev->next = next_info->next;
-      } else if (next_info->next && !next_info->prev) {
-        next_info->next->prev = NULL;
-        head = next_info->next;
-      } else if (!next_info->next && next_info->prev) {
-        next_info->prev->next = NULL;
-      }
-
+      removeLinks(prev_info);
+      removeLinks(next_info);
       current_head = (char*)prev_info - tag_size;
       current_head->size = total_size;
       current_foot->size = total_size;
       current_info = (char*)prev_info;
     } else if (prev_tag->free && !next_tag->free) {
       total_size = current_head->size + prev_tag->size + (tag_size * 2);
-      if (prev_info->next && prev_info->prev) {
-        prev_info->next->prev = prev_info->prev;
-        prev_info->prev->next = prev_info->next;
-      } else if (prev_info->next && !prev_info->prev) {
-        prev_info->next->prev = NULL;
-        head = prev_info->next;
-      } else if (!prev_info->next && prev_info->prev) {
-        prev_info->prev->next = NULL;
-      }
-
+      removeLinks(prev_info);
       current_head = (char*)prev_info - tag_size;
       current_head->size = total_size;
       current_foot->size = total_size;
       current_info = (char*)prev_info;
     } else if (!prev_tag->free && next_tag->free) {
       total_size = current_head->size + next_tag->size + (tag_size * 2);
-      if (next_info->next && next_info->prev) {
-        next_info->next->prev = next_info->prev;
-        next_info->prev->next = next_info->next;
-      } else if (next_info->next && !next_info->prev) {
-        next_info->next->prev = NULL;
-        head = next_info->next;
-      } else if (!next_info->next && next_info->prev) {
-        next_info->prev->next = NULL;
-      }
+      removeLinks(next_info);
       current_head->size = total_size;
       current_foot = (char*)next_info + next_tag->size;
       current_foot->size = total_size;
@@ -258,5 +175,47 @@ void myfree(void *ptr){
   current_info->prev = NULL;
   head->prev = current_info;
   head = current_info;
+  pthread_mutex_unlock(&malloc_lock);
+}
 
+void split(tag *current_head, tag *current_foot, tag *next_head, tag *next_foot, block_info *current_info, block_info *next_info, int size) {
+  next_head = (char*)current_info + (size + tag_size);
+  next_head->size = current_head->size - (size + tag_size * 2);
+  next_head->free = 1;
+  next_foot = current_foot;
+  next_foot->size = next_head->size;
+  next_foot->free = 1;
+  current_head->size = size;
+  current_head->free = 0;
+  current_foot = (char*)next_head - tag_size;
+  current_foot->size = size;
+  current_foot->free = 0;
+  next_info = (char*)next_head + tag_size;
+  if (current_info->next) {
+    next_info->next = current_info->next;
+    current_info->next->prev = next_info;
+  } else {
+    next_info->next = NULL;
+  }
+  if (current_info->prev) {
+    next_info->prev = current_info->prev;
+    current_info->prev->next = next_info;
+  } else {                           //Nothing previous means it is the head
+    next_info->prev = NULL;
+    head = next_info;
+  }
+}
+
+void removeLinks (block_info *link_ptrs) {
+  if (link_ptrs->next && link_ptrs->prev) {
+    link_ptrs->next->prev = link_ptrs->prev;
+    link_ptrs->prev->next = link_ptrs->next;
+  } else if (link_ptrs->next && !link_ptrs->prev) {
+    link_ptrs->next->prev = NULL;
+    head = link_ptrs->next;
+  } else if (!link_ptrs->next && link_ptrs->prev) {
+    link_ptrs->prev->next = NULL;
+  } else {
+    head = NULL;
+  }
 }
